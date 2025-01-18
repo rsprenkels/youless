07e4d2ce1dcb1809455dcc504d78a4c2b96ce0fc
Ron Sprenkels <ron.sprenkels@gmail.com>
2025-01-19 00:25:23 +0100

    initial commit

---
+++ src/youless_dao.py
@@ -0,0 +1,70 @@
+import os
+from datetime import datetime
+import sqlite3
+from time import strftime
+import logging as log
+
+log.basicConfig(format="%(asctime)s - %(message)s", level=log.INFO)
+
+db_fn_default = "data.sqlite"
+
+
+class Dao:
+    def __init__(self, db_file: str = db_fn_default):
+        if os.path.isfile(db_file):
+            self.conn = sqlite3.connect(db_file, check_same_thread=False)
+            print(f"database connected {db_file}")
+        else:
+            self.conn = sqlite3.connect(db_file, check_same_thread=False)
+            self.conn.execute(
+                """
+                create table data (
+                    tm int,
+                    net numeric,
+                    pwr int,
+                    ts0 int,
+                    cs0 numeric,
+                    ps0 int,
+                    p1 numeric,
+                    p2 numeric,
+                    n1 numeric,
+                    n2 numeric,
+                    gas numeric,
+                    gts int,
+                    wtr numeric,
+                    wts int);
+            """
+            )
+            print(f"database created {db_file}")
+
+    def add(self, datagram: dict):
+        tablename = "data"
+        keys = ",".join(datagram.keys())
+        question_marks = ",".join(list("?" * len(datagram)))
+        values = tuple(datagram.values())
+        statement = (
+            "INSERT INTO "
+            + tablename
+            + " ("
+            + keys
+            + ") VALUES ("
+            + question_marks
+            + ")"
+        )
+        self.conn.execute(statement, values)
+        self.conn.commit()
+
+
+def remove_if_exists(filename):
+    if os.path.isfile(filename):
+        os.remove(filename)
+
+
+# https://gist.github.com/pdc/1188720 for mocking time
+
+
+def hidden_test_1():
+    test_db = "test1_dbfile.sqlite"
+    remove_if_exists(test_db)
+    d = Dao(test_db)
+    d.add("test1key", "test1_value")
---
+++ src/youless_reader.py
@@ -0,0 +1,22 @@
+import json
+import time
+import requests
+import youless_dao
+
+
+def youless_reader():
+    dao = youless_dao.Dao()
+    prev_datagram = None
+    while True:
+        r = requests.get("http://192.168.2.12/e")
+        if r.status_code == 200:
+            datagram = json.loads(r.content)[0]
+            if prev_datagram is None or datagram["tm"] != prev_datagram["tm"]:
+                prev_datagram = datagram
+                dao.add(datagram)
+                print(datagram)
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    youless_reader()
